97a05150a99aa7be576463f5d9bb3087544c2630
Rithanya-Sridhar <82592262+Rithanya-Sridhar@users.noreply.github.com>
2021-08-26 09:56:11 +0530

    Add files via upload

--- online jewel ordering system using mean stack/user.js
+++ online jewel ordering system using mean stack/user.js
@@ -1,53 +1,18 @@
-var express = require('express');
-const product = require('../model/product');
-var router = express.Router();
-var csrf=require('csurf');
-const passport = require('passport');
-const { authenticate } = require('passport');
-var csrfProtection=csrf();
-router.use(csrfProtection);
+var mongoose=require('mongoose');
+var bcrypt=require('bcrypt-nodejs');
+var Schema=mongoose.Schema;
+var userschema=new Schema({
+    email:{type:String,required:true},
+    password:{type:String,required:true},
 
-router.get('/profile',isLoggedIn,function(req,res,next){
-  res.render('user/profile');
-});
-router.get('/logout',isLoggedIn,function(req,res,next){
-  req.logOut();
-  res.redirect('/');
-});
-router.use('/',notLoggedIn,function(req,res,next){
-  next();
+
 });
 
-router.get('/signup',function(req,res,next){
-  var messages=req.flash('error');
-  res.render('user/signup',{csrfToken:req.csrfToken(),messages:messages,hasError:messages.length>0});
-});
-router.post('/signup',passport.authenticate('local.signup',{
-  successRedirect:'/user/profile',
-  failureRedirect:'/user/signup',
-  failureFlash:true
-}));
-router.get('/signin',function(req,res,next){
-  var messages=req.flash('error');
-  res.render('user/signin',{csrfToken:req.csrfToken(),messages:messages,hasError:messages.length>0});
-});
-router.post('/signin',passport.authenticate('local.signin',{
-  successRedirect:'/user/profile',
-  failureRedirect:'/user/signin',
-  failureFlash:true
-}));
+userschema.methods.encryptPassword=function(password){
+  return bcrypt.hashSync(password,bcrypt.genSaltSync(5),null);
+};
+userschema.methods.validPassword=function(password){
+  return bcrypt.compareSync(password,this.password);
+};
 
-module.exports = router;
-
-function isLoggedIn(req,res,next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect('/');
-}
-function notLoggedIn(req,res,next){
-  if(!req.isAuthenticated()){
-    return next();
-  }
-  res.redirect('/');
-}
+module.exports=mongoose.model('user',userschema);
